821b72508347fe0d09947d4a86126c48de702012
kenshgy <ken.shgy.1386@gmail.com>
2023-04-03 23:23:14 +0900

    add page

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="align-end text-white"
+      height="200"
+      src="https://images.unsplash.com/photo-1511285560929-80b456fea0bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d2VkZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+      cover
+    >
+      <v-card-title>Schedule</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pt-4">
+      Sunday, Nov xxth
+    </v-card-subtitle>
+
+    <v-card-text>
+      <div>17:40 
+        <a href="https://www.meijikinenkan.gr.jp/wedding/shinzen/jingu-wed.html">
+          Meiji Shrine
+        </a>
+      </div>
+      <div>
+        &nbsp;&nbsp; |
+      </div>
+      <div>21:20</div>
+    </v-card-text>
+
+  </v-card>
 </template>
