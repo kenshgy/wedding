54633bab0079d77da9e62077ae5a4f8d9405cbd4
kenshgy <ken.shgy.1386@gmail.com>
2023-04-04 00:56:00 +0900

    add_title

---
+++ front/src/views/WelcomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <v-card class="mx-auto" max-width="400" style="display: flex;">
+        <router-link to="/home">
+            <img src="../assets/title.gif" alt="GIF" width="400" height="350" />
+        </router-link>
+    </v-card>
+</template>
