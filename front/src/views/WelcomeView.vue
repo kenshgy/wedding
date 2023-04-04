d76e81281d41fc10a1270d3640a9c2d79ed44904
kenshgy <ken.shgy.1386@gmail.com>
2023-04-05 04:30:11 +0900

    update

--- front/src/views/WelcomeView.vue
+++ front/src/views/WelcomeView.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <v-row>
-    <v-col cols="12">
-      <v-card style="display: flex">
-        <router-link to="/home">
-          <img src="../assets/title.gif" width="500" height="500" />
-        </router-link>
-      </v-card>
-    </v-col>
-  </v-row>
+  <router-link to="/home">
+    <img src="../assets/title.gif" width="500" height="500" />
+  </router-link>
 </template>
