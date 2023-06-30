31a3e5db88197a97c8f5be14ad5ed81757c2ca81
kenshgy <ken.shgy.1386@gmail.com>
2023-06-30 19:16:42 +0900

    余計なcssを削除

--- front/src/main.ts
+++ front/src/main.ts
@@ -4,7 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
