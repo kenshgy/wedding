b92e734434cc20e5e93bfb658867c0b4a22c3120
kenshgy <ken.shgy.1386@gmail.com>
2023-07-10 00:50:00 +0900

    update

--- front/src/views/LetterView.vue
+++ front/src/views/LetterView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import LetterCard from '@/components/LetterCard.vue'
 import router from '@/router'
-import { reactive, ref } from 'vue'
-const openLetterFlag = ref<boolean>(false)
 
 async function openLetter() {
   await new Promise((resolve) => setTimeout(resolve, 500))
