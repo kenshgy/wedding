1be393b6bf7caff14a86c0b721a0aa410b8dfc40
kenshgy <ken.shgy.1386@gmail.com>
2023-07-17 22:18:00 +0900

    Revert "Revert "画面リダイレクトせずに手紙を開く""
    This reverts commit ba4cf11e84a31574476d5de47d734761224b52cc.

--- front/src/components/InvitationContents.vue
+++ front/src/components/InvitationContents.vue
@@ -8,22 +8,18 @@ import CountDown from '@/components/CountDown.vue'
 </script>
 
 <template>
-  <v-card class="pa-4">
-    <v-row>
-      <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-        <CarouselCard />
-      </v-col>
-      <v-col cols="12" lg="6" md="6" sm="12" xs="12">
-        <MessageCard />
-      </v-col>
-    </v-row>
-  </v-card>
+  <v-row>
+    <v-col cols="12" lg="6" md="6" sm="12" xs="12">
+      <CarouselCard />
+    </v-col>
+    <v-col cols="12" lg="6" md="6" sm="12" xs="12">
+      <MessageCard />
+    </v-col>
+  </v-row>
   <CountDown class="mt-4" />
   <ScheduleCard />
-  <v-card>
-    <LocationCard />
-    <FormCard />
-  </v-card>
+  <LocationCard />
+  <FormCard />
 </template>
 
 <style scoped>
