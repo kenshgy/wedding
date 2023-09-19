320754d80ea4c2ff4dabeaf440fa8080e34f2a93
kenshgy <ken.shgy.1386@gmail.com>
2023-09-20 00:14:09 +0900

    メッセージ修正

--- front/src/components/ScheduleCard.vue
+++ front/src/components/ScheduleCard.vue
@@ -27,7 +27,6 @@ const props = defineProps({
             {{ item.title }}
             <a v-if="item.locationLink !== ''" :href="item.locationLink"> ＠{{ item.location }} </a>
             <br />
-            <!-- <p v-if="item.comment !== []">{{ item.comment }}</p> -->
             <div v-for="(comment, i) in item.comment" :key="i">
               <p>{{ comment }}</p>
             </div>
