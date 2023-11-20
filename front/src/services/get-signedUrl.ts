119c7ff3d4c6f237b066b2c69cbf2933d65f787f
kenshgy <ken.shgy.1386@gmail.com>
2023-11-21 01:06:07 +0900

    CORS対応

--- front/src/services/get-signedUrl.ts
+++ front/src/services/get-signedUrl.ts
@@ -2,7 +2,7 @@ import http from "../http-common";
 
 class GetSignedUrl {
   async get(fileName: string) {
-    const result = await http.get("https://pocif7c33c.execute-api.ap-northeast-1.amazonaws.com/dev/files/signedUrl?fileName=" + fileName);
+    const result = await http.get("api/files/signedUrl?fileName=" + fileName);
     return result.data;
   }
 }
