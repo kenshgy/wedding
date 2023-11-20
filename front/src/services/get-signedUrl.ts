644aa7b0e020e7d898b9d801accdf625a98acaac
kenshgy <ken.shgy.1386@gmail.com>
2023-11-21 01:32:11 +0900

    cnosole,log追加

--- front/src/services/get-signedUrl.ts
+++ front/src/services/get-signedUrl.ts
@@ -2,7 +2,8 @@ import http from "../http-common";
 
 class GetSignedUrl {
   async get(fileName: string) {
-    const result = await http.get("api/files/signedUrl?fileName=" + fileName);
+    const result = await http.get("/api/files/signedUrl?fileName=" + fileName);
+    console.log(result)
     return result.data;
   }
 }
